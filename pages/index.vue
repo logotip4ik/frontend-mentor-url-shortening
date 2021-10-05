5420e8b34e53757e95599a444551a5cda53d1388
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-10-05 21:36:46 +0300

    update: added functions and data for containing links

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,37 @@
   <div>
     <Navbar></Navbar>
     <Header></Header>
-    <Shortener></Shortener>
+    <Shortener :links="links" @create-link="createLink"></Shortener>
   </div>
 </template>
 
 <script>
-export default {}
+const COOKIE_NAME = '__shortly__nuxt__'
+
+export default {
+  asyncData({ $cookies }) {
+    const allCookies = $cookies.getAll()
+    const allRedirectCookies = Object.keys(allCookies).filter(
+      (key) => key.slice(0, COOKIE_NAME.length) === COOKIE_NAME
+    )
+
+    const links = allRedirectCookies.map((cookie) => allCookies[cookie])
+
+    return { links }
+  },
+  methods: {
+    createLink(linkObj) {
+      const oneMonth = 60 * 60 * 24 * 31
+      this.links.unshift(linkObj)
+      this.$cookies.set(
+        `${COOKIE_NAME}${linkObj.id}`,
+        JSON.stringify(linkObj),
+        {
+          maxAge: oneMonth,
+          sameSite: 'strict',
+        }
+      )
+    },
+  },
+}
 </script>
