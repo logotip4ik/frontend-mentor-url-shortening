fe7f7aaf95b6e75e85e72234ea53b1645eabb71d
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-10-05 16:35:01 +0300

    update: added redirect page

---
+++ pages/_id.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>REDIRECTING...</h1>
+</template>
+
+<script>
+const COOKIE_NAME = '__shortly__nuxt__'
+
+export default {
+  async asyncData({ params, $cookies, redirect }) {
+    const { link } = await $cookies.get(`${COOKIE_NAME}${params.id}`)
+    redirect(link)
+  },
+}
+</script>
