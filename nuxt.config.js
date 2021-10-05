631d3eb8ebac7a51420d39fe9949b877406fb7b3
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-10-05 15:58:55 +0300

    update: added cookies to modules in nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,7 +42,7 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: ['cookie-universal-nuxt'],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
