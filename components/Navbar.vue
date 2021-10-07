1db4055850f3f279b9bc0ee52911847414b55feb
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-10-07 13:45:17 +0300

    update: added burger for nav

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -8,6 +8,11 @@
         <li class="nav__list__item">Resources</li>
       </ul>
     </div>
+    <div class="nav__burger">
+      <span class="nav__burger__line"></span>
+      <span class="nav__burger__line"></span>
+      <span class="nav__burger__line"></span>
+    </div>
     <div class="nav__wrapper">
       <button class="btn btn--outlined">Login</button>
       <button class="btn">Sign Up</button>
@@ -62,6 +67,47 @@ export default {}
         color: var(--primary-dark-violate);
         transition: none;
       }
+    }
+  }
+
+  &__burger {
+    --burger-gap: 0.35rem;
+
+    display: none;
+    flex-direction: column;
+    justify-content: center;
+    align-items: stretch;
+    gap: var(--burger-gap);
+
+    width: 30px;
+
+    cursor: pointer;
+
+    &__line {
+      width: 100%;
+      height: 3px;
+
+      background-color: var(--neutral-grayish-violet);
+      border-radius: 0.125rem;
+
+      transition: background-color 0.4s ease;
+    }
+
+    &:hover &__line {
+      background-color: var(--neutral-very-dark-blue);
+      transition: none;
+    }
+  }
+
+  @media screen and (max-width: 685px) {
+    &__list {
+      display: none;
+    }
+    &__burger {
+      display: flex;
+    }
+    &__wrapper:last-child {
+      display: none;
     }
   }
 }
